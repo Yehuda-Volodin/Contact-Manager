14aae9f801f6a5b6e3f0e5c75df445878dc05f3e
Yehuda Volodin <falcon81@tut.by>
2020-06-16 23:44:22 +0300

    Updates

---
+++ precache-manifest.fd6ab04971bb14a66978a2ddee568d5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff40df28bd9b47011a1053fa48aaf63",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "2b5827f137357d5fe54e",
+    "url": "/Contact-Manager/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4b48ed25b12d23baf12b",
+    "url": "/Contact-Manager/static/css/main.ebdb694d.chunk.css"
+  },
+  {
+    "revision": "2b5827f137357d5fe54e",
+    "url": "/Contact-Manager/static/js/2.5fc43e65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Contact-Manager/static/js/2.5fc43e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b48ed25b12d23baf12b",
+    "url": "/Contact-Manager/static/js/main.be306bab.chunk.js"
+  },
+  {
+    "revision": "bffdad65727f70aa0d62",
+    "url": "/Contact-Manager/static/js/runtime-main.f4e41be6.js"
+  },
+  {
+    "revision": "3b6d08b0930dbb80382ee690cc4b89b7",
+    "url": "/Contact-Manager/static/media/defaultBg.3b6d08b0.jpg"
+  }
+]);
